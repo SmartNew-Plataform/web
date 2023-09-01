f5f45adb006cb70a3c0a12b1606f6fc6ad8800a7
Davi Rezende <daviresendes12@gmail.com>
2023-09-01 14:09:19 -0300

    update(modules): add icons

--- src/components/modules.tsx
+++ src/components/modules.tsx
@@ -2,15 +2,38 @@
 
 import { ModuleButton } from '@/components/module-button'
 import { useUserStore } from '@/store/user-store'
+import dynamicIconImports from 'lucide-react/dynamicIconImports'
+
+const moduleIcons = {
+  'flaticon-cogwheel-2': 'cog',
+  'flaticon-stopwatch': 'list-todo',
+  'flaticon-app': 'fuel',
+  'flaticon-truck': 'bus',
+  'flaticon2-protection': 'shield-check',
+  'flaticon-coins': 'file-edit',
+  // 'flaticon-coins': 'square',
+  'flaticon-open-box': 'shopping-bag',
+  '': 'landmark',
+  'flaticon2-checklist': 'list-checks',
+}
+
+type IconNameType = keyof typeof moduleIcons
 
 export function Modules() {
   const { modules } = useUserStore()
 
   return (
     <nav className="flex flex-col gap-2">
-      {modules?.map((module) => (
-        <ModuleButton key={module.id} {...module} icon="square" />
-      ))}
+      {modules?.map((module) => {
+        const iconIndex: IconNameType = module.icon.split(
+          ' ',
+        )[0] as IconNameType
+
+        const currentIcon = moduleIcons[
+          iconIndex
+        ] as keyof typeof dynamicIconImports
+        return <ModuleButton key={module.id} {...module} icon={currentIcon} />
+      })}
     </nav>
   )
 }
