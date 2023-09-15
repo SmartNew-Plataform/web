922909d9a76ab0560c90f6647b097bad4f935072
Davi Rezende <daviresendes12@gmail.com>
2023-09-15 10:28:03 -0300

    style: change initial text

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation'
 export default function App() {
   redirect('https://www.smartnewsystem.com.br/erp/Menu-v2/')
 
-  return <h1>App</h1>
+  return <h1>SmartNew Sistemas</h1>
 }
