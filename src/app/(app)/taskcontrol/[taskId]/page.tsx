32300aed0029a295a2c1f7901b93f250f27606bd
Davi Rezende <daviresendes12@gmail.com>
2023-10-03 23:53:45 -0300

    update(taskcontrol): implements api

--- src/app/(app)/taskcontrol/[taskId]/page.tsx
+++ src/app/(app)/taskcontrol/[taskId]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Card } from '@/components/ui/card'
-import { SheetEditTask } from './ sheet-edit-task'
+
 import { GridSubtasks } from './grid-subtasks'
+import { HeaderSubtasks } from './header-subtasks'
 
 interface TaskpageProps {
   params: {
@@ -11,17 +11,8 @@ interface TaskpageProps {
 
 export default function TaskPage({ params }: TaskpageProps) {
   return (
-    <div className="flex h-full flex-col gap-4 p-4">
-      <Card className="flex items-center justify-between p-4">
-        <div className="flex flex-col justify-start">
-          <h1 className="text-2xl font-bold text-slate-700">Titulo da task</h1>
-          <span className="max-w-min rounded bg-slate-200 px-2 py-1 uppercase text-slate-600">
-            aberto
-          </span>
-        </div>
-
-        <SheetEditTask />
-      </Card>
+    <div className="flex h-full flex-col gap-4 p-4 pt-0">
+      <HeaderSubtasks />
 
       <GridSubtasks />
     </div>
