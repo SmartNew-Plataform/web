2c2684a85c06ed17bd00ae8ec7eee261397825ac
Davi Rezende <daviresendes12@gmail.com>
2023-11-14 10:38:21 -0300

    update: add memo function in module button

--- src/components/modules.tsx
+++ src/components/modules.tsx
@@ -52,17 +52,3 @@ export function Modules() {
     </nav>
   )
 }
-
-// eslint-disable-next-line no-lone-blocks
-{
-  /* <ModuleButton icon="shopping-bag" name="Compras" />
-<ModuleButton icon="shopping-cart" name="Vendas" />
-<ModuleButton icon="landmark" name="Financeiro" />
-<ModuleButton icon="orbit" name="Telemetria" />
-<ModuleButton icon="list-todo" name="Task Control" />
-<ModuleButton icon="bus" name="Produção" />
-<ModuleButton icon="cog" name="Manutenção" />
-<ModuleButton icon="fuel" name="Controle de Abastecimento" />
-<ModuleButton icon="shield-check" name="Security" />
-<ModuleButton icon="file-edit" name="Contratos" /> */
-}
