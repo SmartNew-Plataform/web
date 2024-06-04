f98721f02ee9043fc0a9518b9e19e1931c5a3779
Davi Rezende <daviresendes12@gmail.com>
2024-06-04 15:31:59 -0300

    feat: add qr code generator in diverse screen

--- src/app/(app)/checklist/diverse/diverse-modal.tsx
+++ src/app/(app)/checklist/diverse/diverse-modal.tsx
@@ -42,6 +42,7 @@ export function DiverseModal({ mode, ...props }: DiverseModalProps) {
     reset({
       branch: editingData?.branchId.toString(),
       description: editingData?.text,
+      tag: editingData?.tag,
     })
   }, [editingData])
 
