f89e8a325dd9c7f31acc9c9f9f681b9cebf46802
GunTsy <carlosalexandre3254@gmail.com>
2024-07-11 12:16:48 -0300

    create fuelling header

---
+++ src/app/(app)/abastecimento/header-fuelling.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { Header } from '@/components/header'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useUserStore } from '@/store/user-store'
+import { AreaChart, Plus, XCircle } from 'lucide-react'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+export const revalidate = true
+
+export function HeaderFuelling() {
+  const searchParams = useSearchParams()
+  const { modules } = useUserStore(({ modules }) => ({
+    modules,
+  }))
+
+  if (!modules) return
+  return (
+    <Header className="sticky top-0 z-30">
+      <div className="flex gap-6">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <Plus className="h-4 w-4" />
+              Cadastro
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <Link
+                target="_blank"
+                href={{
+                  pathname: '/abastecimento/create/tank',
+                  query: { token: searchParams.get('token') },
+                }}
+              >
+                Tanque
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link
+                target="_blank"
+                href={{
+                  pathname: '/abastecimento/create/comboio',
+                  query: { token: searchParams.get('token') },
+                }}
+              >
+                Comboio
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Button asChild variant="ghost">
+          <Link
+            target="_blank"
+            href={{
+              pathname: '/abastecimento/fuel-inlet',
+              query: { token: searchParams.get('token') },
+            }}
+          >
+            <AreaChart className="h-4 w-4" />
+            Entradas
+          </Link>
+        </Button>
+
+        <Button asChild variant="ghost">
+          <Link
+            target="_blank"
+            href={{
+              pathname: '/abastecimento/new-fuelling',
+              query: { token: searchParams.get('token') },
+            }}
+          >
+            <XCircle className="h-4 w-4" />
+            Abastecimentos
+          </Link>
+        </Button>
+      </div>
+    </Header>
+  )
+}
