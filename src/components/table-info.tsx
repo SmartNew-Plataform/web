f7991d0134e58429d0c1e06076c6d1006d70882a
GunTsy <carlosalexandre3254@gmail.com>
2024-08-26 15:48:51 -0300

    create function delet item and filter table

--- src/components/table-info.tsx
+++ src/components/table-info.tsx
@@ -4,10 +4,15 @@ import { columns } from '@/components/columns/columns-info'
 import { DataTableServerPagination } from '@/components/data-table-server-pagination'
 import { api } from '@/lib/api'
 import { useCoreScreensStore } from '@/store/core-screens-store'
+import { useState } from 'react'
+import { AlertModal } from './alert-modal'
 
 export function TableInfo() {
   const { infoScreen } = useCoreScreensStore()
   // console.log('columns => ', columns)
+
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null)
+
   async function fetchDataTable(params: {
     index: number
     perPage: number
@@ -31,12 +36,37 @@ export function TableInfo() {
     return data
   }
 
+  async function handleDelete() {
+    if (selectedItemId !== null) {
+      try {
+        await api.delete(`/smart-list/check-list/${selectedItemId}`)
+      } catch (error) {
+        console.error('Failed to delete:', error)
+      }
+      setSelectedItemId(null)
+    }
+  }
+
+  const handleDeleteClick = (id: number) => {
+    setSelectedItemId(id)
+  }
+
+  const tableColumns = columns(handleDeleteClick)
+
   return (
-    <DataTableServerPagination
-      id="checklist-table"
-      columns={columns}
-      fetchData={fetchDataTable}
-      filterText={infoScreen?.filter?.filterText}
-    />
+    <>
+      <DataTableServerPagination
+        id="checklist-table"
+        columns={tableColumns}
+        fetchData={fetchDataTable}
+        filterText={infoScreen?.filter?.filterText}
+      />
+
+      <AlertModal
+        open={selectedItemId !== null}
+        onConfirm={handleDelete}
+        onOpenChange={(open) => !open && setSelectedItemId(null)}
+      />
+    </>
   )
 }
