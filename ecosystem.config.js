29959a225e53e3ee46c50ea47ff9097a527a4ed2
Ruben Borges <ruben.gyn@gmail.com>
2024-10-11 17:27:05 -0300

    update file config ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,23 @@ module.exports = {
     apps : [
         {
           name: "smart-homolog",
-          script: "npm start",
+          script: "npm run start:homolog",
           watch: true,
           env: {
               "PORT": 3033,
-              "NODE_ENV": "development"
+              "NODE_ENV": "development",
+              "NEXT_PUBLIC_API_URL":"https://api-homolog.smartnewservices.com.br"
           },
-          env_production: {
-              "PORT": 3032,
-              "NODE_ENV": "production",
+        },
+        {
+            name: "smart",
+            script: "npm run start:prod",
+            watch: true,
+            env: {
+                "PORT": 3032,
+                "NODE_ENV": "production",
+                "NEXT_PUBLIC_API_URL":"https://api.smartnewservices.com.br"
+            },
           }
-        }
     ]
   }
