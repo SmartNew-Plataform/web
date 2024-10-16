21689d76c4729307c3d3496442d72ac390bf2928
Davi Rezende <daviresendes12@gmail.com>
2024-10-16 11:28:35 -0300

    feat: add switch mode view

---
+++ src/app/(app)/manutencao/order-service/kanban-view.tsx
@@ -0,0 +1,3 @@
+export function KanbanView() {
+  return <div>kanban</div>
+}
--- src/app/(app)/manutencao/order-service/page.tsx
+++ src/app/(app)/manutencao/order-service/page.tsx
@@ -1,12 +1,13 @@
 import { PageWrapper } from '@/components/page-wrapper'
 import { Header } from './header'
-import { TableServiceOrder } from './table-service-order'
+import { Wrapper } from './wrapper'
 
 export default function ImportationPage() {
   return (
     <PageWrapper>
       <Header />
-      <TableServiceOrder />
+
+      <Wrapper />
     </PageWrapper>
   )
 }
