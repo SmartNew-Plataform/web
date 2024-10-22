fb350d647b6b85c05d1bf546fba8608c190b0e1c
Davi Rezende <daviresendes12@gmail.com>
2024-10-22 18:03:53 -0300

    fix(kanban): modal not working

--- src/app/(app)/manutencao/order-service/kanban-view.tsx
+++ src/app/(app)/manutencao/order-service/kanban-view.tsx
@@ -1,109 +1,146 @@
-import React, { useEffect, useState } from 'react';
-import { useServiceOrder, ServiceOrder, StatusFilterData } from '@/store/maintenance/service-order';
-import { DragDropContext, Droppable, Draggable, DropResult, DroppableProvided, DraggableProvided } from 'react-beautiful-dnd';
-import { useQueryClient } from '@tanstack/react-query';
-import { FiCheckCircle, FiAlertCircle, FiPauseCircle, FiPlus, FiCheck, FiInfo, FiMail, FiLock, FiSettings, FiHelpCircle } from 'react-icons/fi'; 
-import { useRouter, useSearchParams } from 'next/navigation';
-import Modal from 'react-modal';
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
+import {
+  ServiceOrder,
+  StatusFilterData,
+  useServiceOrder,
+} from '@/store/maintenance/service-order'
+import { useQueryClient } from '@tanstack/react-query'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import {
+  DragDropContext,
+  Draggable,
+  DraggableProvided,
+  DropResult,
+  Droppable,
+  DroppableProvided,
+} from 'react-beautiful-dnd'
+import {
+  FiAlertCircle,
+  FiCheck,
+  FiCheckCircle,
+  FiHelpCircle,
+  FiInfo,
+  FiLock,
+  FiMail,
+  FiPauseCircle,
+  FiPlus,
+  FiSettings,
+} from 'react-icons/fi'
+// import Modal from 'react-modal'
 
 const statusColors: { [key: string]: string } = {
   'EM ABERTO': 'border-red-500',
-  'DISPONIVEL': 'border-green-500',
+  DISPONIVEL: 'border-green-500',
   'EM ANDAMENTO': 'border-yellow-500',
-  'AUDITORIA': 'border-blue-500',
-  'ENCERRADO': 'border-black-500',
+  AUDITORIA: 'border-blue-500',
+  ENCERRADO: 'border-black-500',
   'AUSÊNCIA DE MATERIAL': 'border-orange-500',
-  'REPROVADO': 'border-red-500',
+  REPROVADO: 'border-red-500',
   'EM MANUTENÇÃO EXTERNA': 'border-purple-500',
-};
+}
 
 const statusIcons: { [key: string]: React.ReactNode } = {
   'EM ABERTO': <FiAlertCircle className="text-red-500" />,
-  'DISPONIVEL': <FiCheckCircle className="text-green-500" />,
+  DISPONIVEL: <FiCheckCircle className="text-green-500" />,
   'EM ANDAMENTO': <FiPauseCircle className="text-yellow-500" />,
-  'AUDITORIA': <FiMail className="text-blue-500" />,
-  'ENCERRADO': <FiLock className="text-black-500" />,
+  AUDITORIA: <FiMail className="text-blue-500" />,
+  ENCERRADO: <FiLock className="text-black-500" />,
   'AUSÊNCIA DE MATERIAL': <FiHelpCircle className="text-orange-500" />,
-  'REPROVADO': <FiAlertCircle className="text-red-500" />,
-  'EM MANUTENÇÃO EXTERNA': <FiSettings className="text-purple-500" />
-};
+  REPROVADO: <FiAlertCircle className="text-red-500" />,
+  'EM MANUTENÇÃO EXTERNA': <FiSettings className="text-purple-500" />,
+}
 
 const KanbanView = () => {
-  const { serviceOrders, fetchServiceOrders, updateServiceOrderStatus, selects } = useServiceOrder();
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentOrderId, setCurrentOrderId] = useState<number | null>(null);
-  const [newStatusId, setNewStatusId] = useState<number | null>(null);
-  const [justify, setJustify] = useState<string>('');
-  const [dateEnd, setDateEnd] = useState<string | null>(null);
+  const {
+    serviceOrders,
+    fetchServiceOrders,
+    updateServiceOrderStatus,
+    selects,
+  } = useServiceOrder()
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentOrderId, setCurrentOrderId] = useState<number | null>(null)
+  const [newStatusId, setNewStatusId] = useState<number | null>(null)
+  const [justify, setJustify] = useState<string>('')
+  const [dateEnd, setDateEnd] = useState<string | null>(null)
 
   useEffect(() => {
-    fetchServiceOrders();
-  }, [fetchServiceOrders]);
+    fetchServiceOrders()
+  }, [fetchServiceOrders])
 
   const onDragEnd = (result: DropResult) => {
-    const { destination, source, draggableId } = result;
-    if (!destination) return;
-  
-    const orderId = parseInt(draggableId);
-    const newStatusId = parseInt(destination.droppableId);
+    const { destination, source, draggableId } = result
+    if (!destination) return
+
+    const orderId = parseInt(draggableId)
+    const newStatusId = parseInt(destination.droppableId)
 
     if (destination.droppableId !== source.droppableId) {
+      const draggedOrder = statusList?.find(
+        ({ id }) => id === destination.droppableId,
+      )
+      console.log(draggedOrder?.hasJustify, draggedOrder?.hasFinished)
 
-      const draggedOrder = serviceOrders?.find(order => order.id === orderId);
-      if (draggedOrder?.statusOrderService.hasJustify || draggedOrder?.statusOrderService.hasFinished) {
-        setCurrentOrderId(orderId);
-        setNewStatusId(newStatusId);
-        setIsModalOpen(true); 
+      if (draggedOrder?.hasJustify || draggedOrder?.hasFinished) {
+        setCurrentOrderId(orderId)
+        setNewStatusId(newStatusId)
+        setIsModalOpen(true)
       } else {
-
         updateServiceOrderStatus(orderId, newStatusId).then(() => {
-          queryClient.refetchQueries(['maintenance-service-order-table']);
-        });
+          queryClient.refetchQueries(['maintenance-service-order-table'])
+        })
       }
     }
-  };
+  }
 
   const openOrderDetails = (orderId: number) => {
     if (searchParams && searchParams.get('token')) {
       router.push(
-        `/manutencao/order-service/${orderId}/details?token=${searchParams.get('token')}&h=hidden`
-      );
+        `/manutencao/order-service/${orderId}/details?token=${searchParams.get('token')}&h=hidden`,
+      )
     }
-  };
-
+  }
 
   const handleModalSubmit = () => {
     if (currentOrderId && newStatusId !== null) {
-      updateServiceOrderStatus(currentOrderId, newStatusId, { justify, dateEnd }).then(() => {
-        queryClient.refetchQueries(['maintenance-service-order-table']);
-        closeModal();
-      });
+      updateServiceOrderStatus(currentOrderId, newStatusId, {
+        justify,
+        dateEnd,
+      }).then(() => {
+        queryClient.refetchQueries(['maintenance-service-order-table'])
+        closeModal()
+      })
     }
-  };
-
+  }
 
   const closeModal = () => {
-    setIsModalOpen(false);
-    setJustify('');
-    setDateEnd(null);
-  };
+    setIsModalOpen(false)
+    setJustify('')
+    setDateEnd(null)
+  }
 
   const groupedOrders: { [key: string]: ServiceOrder[] } = serviceOrders
-    ? serviceOrders.reduce((acc, order) => {
-        const statusId = order.statusOrderService.id.toString();
-        if (!acc[statusId]) {
-          acc[statusId] = [];
-        }
-        acc[statusId].push(order);
-        return acc;
-      }, {} as { [key: string]: ServiceOrder[] })
-    : {};
+    ? serviceOrders.reduce(
+        (acc, order) => {
+          const statusId = order.statusOrderService.id.toString()
+          if (!acc[statusId]) {
+            acc[statusId] = []
+          }
+          acc[statusId].push(order)
+          return acc
+        },
+        {} as { [key: string]: ServiceOrder[] },
+      )
+    : {}
 
-  const mapSelectDataToStatusFilter = (selectData: any[]): StatusFilterData[] => {
+  const mapSelectDataToStatusFilter = (
+    selectData: any[],
+  ): StatusFilterData[] => {
     return selectData.map((status) => ({
       id: status.value || status.id,
       name: status.label || status.name,
@@ -111,98 +148,129 @@ const KanbanView = () => {
       hasFinished: status.hasFinished,
       hasJustify: status.hasJustify,
       count: 0,
-    }));
-  };
+    }))
+  }
 
-  const statusList = selects.status ? mapSelectDataToStatusFilter(selects.status) : [];
+  const statusList = selects.status
+    ? mapSelectDataToStatusFilter(selects.status)
+    : []
+
+  console.log(statusList)
 
   return (
     <>
       <DragDropContext onDragEnd={onDragEnd}>
-        <div className="flex gap-6 p-5 overflow-x-auto bg-white bg-opacity-90 rounded-lg">
-          {statusList.map((status: StatusFilterData) => (
-            status.id ? ( 
+        <div className="flex gap-6 overflow-x-auto rounded-lg bg-white bg-opacity-90 p-5">
+          {statusList.map((status: StatusFilterData) =>
+            status.id ? (
               <Droppable key={status.id} droppableId={status.id.toString()}>
                 {(provided: DroppableProvided) => (
                   <div
-                    className="min-w-[280px] flex flex-col bg-transparent gap-2"
+                    className="flex min-w-[280px] flex-col gap-2 bg-transparent"
                     ref={provided.innerRef}
                     {...provided.droppableProps}
                   >
-                    <div className="flex justify-between items-center mb-2">
-                      <div className="text-gray-900 font-bold text-md flex items-center gap-2">
-                        {statusIcons[status.name] || <FiCheckCircle className="text-gray-500" />}
+                    <div className="mb-2 flex items-center justify-between">
+                      <div className="text-md flex items-center gap-2 font-bold text-gray-900">
+                        {statusIcons[status.name] || (
+                          <FiCheckCircle className="text-gray-500" />
+                        )}
                         {status.name}
                       </div>
-                      <FiPlus className="text-black cursor-pointer hover:text-gray-600" />
+                      <FiPlus className="cursor-pointer text-black hover:text-gray-600" />
                     </div>
                     <div className="flex flex-col gap-4">
-                      {groupedOrders[status.id] && groupedOrders[status.id].map((order, index) => (
-                        <Draggable key={order.id.toString()} draggableId={order.id.toString()} index={index}>
-                          {(provided: DraggableProvided) => (
-                            <div
-                              className={`bg-white rounded-lg p-3 shadow-md cursor-grab flex flex-col gap-3 border-l-4 ${statusColors[status.name]}`}
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                            >
-                              <div className="flex justify-between items-center">
-                                <p className="text-gray-700 text-xs"><strong>OS:</strong> {order.codeServiceOrder}</p>
-                                <div className="text-right">
-                                  <FiCheck className="text-green-500 cursor-pointer hover:text-green-700"/>
-                                  <FiInfo className="text-blue-500 cursor-pointer hover:text-blue-700" onClick={() => openOrderDetails(order.id)} />
+                      {groupedOrders[status.id] &&
+                        groupedOrders[status.id].map((order, index) => (
+                          <Draggable
+                            key={order.id.toString()}
+                            draggableId={order.id.toString()}
+                            index={index}
+                          >
+                            {(provided: DraggableProvided) => (
+                              <div
+                                className={`flex cursor-grab flex-col gap-3 rounded-lg border-l-4 bg-white p-3 shadow-md ${statusColors[status.name]}`}
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <div className="flex items-center justify-between">
+                                  <p className="text-xs text-gray-700">
+                                    <strong>OS:</strong>{' '}
+                                    {order.codeServiceOrder}
+                                  </p>
+                                  <div className="text-right">
+                                    <FiCheck className="cursor-pointer text-green-500 hover:text-green-700" />
+                                    <FiInfo
+                                      className="cursor-pointer text-blue-500 hover:text-blue-700"
+                                      onClick={() => openOrderDetails(order.id)}
+                                    />
+                                  </div>
                                 </div>
+                                <p className="text-xs text-gray-700">
+                                  <strong>Cliente:</strong>{' '}
+                                  {order.branch.companyName}
+                                </p>
+                                <p className="text-xs text-gray-700">
+                                  <strong>Equipamento:</strong>{' '}
+                                  {order.equipment}
+                                </p>
+                                <p className="text-xs text-gray-700">
+                                  <strong>Data Programada:</strong>{' '}
+                                  {order.datePrev}
+                                </p>
+                                <p className="text-xs text-gray-700">
+                                  <strong>Data de Emissao:</strong>{' '}
+                                  {order.dateEmission}
+                                </p>
                               </div>
-                              <p className="text-gray-700 text-xs"><strong>Cliente:</strong> {order.branch.companyName}</p>
-                              <p className="text-gray-700 text-xs"><strong>Equipamento:</strong> {order.equipment}</p>
-                              <p className="text-gray-700 text-xs"><strong>Data Programada:</strong> {order.datePrev}</p>
-                              <p className="text-gray-700 text-xs"><strong>Data de Emissao:</strong> {order.dateEmission}</p>
-                            </div>
-                          )}
-                        </Draggable>
-                      ))}
+                            )}
+                          </Draggable>
+                        ))}
                       {provided.placeholder}
                     </div>
                   </div>
                 )}
               </Droppable>
-            ) : null 
-          ))}
+            ) : null,
+          )}
         </div>
       </DragDropContext>
 
-
-      <Modal
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent>
+          <h2>Preencha os detalhes</h2>
+          <label>
+            Justificativa:
+            <input
+              type="text"
+              value={justify}
+              onChange={(e) => setJustify(e.target.value)}
+              className="input-class"
+            />
+          </label>
+          <label>
+            Data de Encerramento:
+            <input
+              type="datetime-local"
+              value={dateEnd || ''}
+              onChange={(e) => setDateEnd(e.target.value)}
+              className="input-class"
+            />
+          </label>
+          <Button onClick={handleModalSubmit}>Enviar</Button>
+        </DialogContent>
+      </Dialog>
+      {/* <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         contentLabel="Justificativa e Data de Encerramento"
         className="modal-class"
         overlayClassName="overlay-class"
       >
-        <h2>Preencha os detalhes</h2>
-        <label>
-          Justificativa:
-          <input
-            type="text"
-            value={justify}
-            onChange={(e) => setJustify(e.target.value)}
-            className="input-class"
-          />
-        </label>
-        <label>
-          Data de Encerramento:
-          <input
-            type="datetime-local"
-            value={dateEnd || ''}
-            onChange={(e) => setDateEnd(e.target.value)}
-            className="input-class"
-          />
-        </label>
-        <button onClick={handleModalSubmit} className="btn-class">Enviar</button>
-        <button onClick={closeModal} className="btn-class">Cancelar</button>
-      </Modal>
+      </Modal> */}
     </>
-  );
-};
+  )
+}
 
-export default KanbanView;
+export default KanbanView
--- src/store/maintenance/service-order.ts
+++ src/store/maintenance/service-order.ts
@@ -1,77 +1,85 @@
-import { SelectData } from '@/@types/select-data';
-import { api } from '@/lib/api';
-import { create } from 'zustand';
+import { SelectData } from '@/@types/select-data'
+import { api } from '@/lib/api'
+import { create } from 'zustand'
 
 type EquipmentData = {
-  branch: SelectData;
-} & SelectData;
+  branch: SelectData
+} & SelectData
 
 export type ServiceOrder = {
-  id: number;
-  codeServiceOrder: string;
-  equipment: string;
-  requester: string;
-  dateTimeRequest: string;
-  dateEmission: string;
-  requestDate: string;
-  datePrev: string;
+  id: number
+  codeServiceOrder: string
+  equipment: string
+  requester: string
+  dateTimeRequest: string
+  dateEmission: string
+  requestDate: string
+  datePrev: string
   branch: {
-    id: number;
-    branchNumber: string;
-    companyName: string;
-  };
+    id: number
+    branchNumber: string
+    companyName: string
+  }
+  status: {
+    value: number
+    label: string
+    color: string
+    hasFinished: boolean
+    hasJustify: boolean
+  }
+
   statusOrderService: {
-    id: number;
-    status: string;
-    color: string;
-    hasFinished: boolean;
-    hasJustify: boolean;
-  };
+    id: number
+    status: string
+    color: string
+    hasFinished: boolean
+    hasJustify: boolean
+  }
   typeMaintenance: {
-    id: number;
-    label: string;
-    typeMaintenance: string;
-  };
-};
+    id: number
+    label: string
+    typeMaintenance: string
+  }
+}
 
 export type StatusFilterData = {
-  id: string;
-  name: string;
-  color: string;
-  hasFinished?: boolean;
-  hasJustify?: boolean;
-  count: number;
-};
+  id: string
+  name: string
+  color: string
+  hasFinished?: boolean
+  hasJustify?: boolean
+  count: number
+}
 
 interface ServiceOrderStoreData {
   selects: {
-    equipment?: EquipmentData[] | undefined;
-    branch?: SelectData[] | undefined;
-    typeMaintenance?: SelectData[] | undefined;
-    maintenanceSector: SelectData[] | undefined;
-    requester: SelectData[] | undefined;
-    status: StatusFilterData[] | undefined; // Atualizado
-    maintainers: SelectData[] | undefined;
-  };
-  statusFilterValue: string | undefined;
-  setStatusFilterValue: (data: string | undefined) => void;
+    equipment?: EquipmentData[] | undefined
+    branch?: SelectData[] | undefined
+    typeMaintenance?: SelectData[] | undefined
+    maintenanceSector: SelectData[] | undefined
+    requester: SelectData[] | undefined
+    status: StatusFilterData[] | undefined // Atualizado
+    maintainers: SelectData[] | undefined
+  }
+  statusFilterValue: string | undefined
+  setStatusFilterValue: (data: string | undefined) => void
 
-  serviceOrders: ServiceOrder[] | undefined;
-  fetchServiceOrders: () => Promise<void>;
+  serviceOrders: ServiceOrder[] | undefined
+  fetchServiceOrders: () => Promise<void>
 
-  statusFilterData: StatusFilterData[] | undefined;
-  setStatusFilterData: (data: StatusFilterData[] | undefined) => void;
+  statusFilterData: StatusFilterData[] | undefined
+  setStatusFilterData: (data: StatusFilterData[] | undefined) => void
   updateServiceOrderStatus: (
     orderId: number,
     newStatusId: number,
-    additionalData?: { justify?: string; dateEnd?: string | null }
-  ) => Promise<void>;
+    additionalData?: { justify?: string; dateEnd?: string | null },
+  ) => Promise<void>
 
-  setSelects: (data: ServiceOrderStoreData['selects']) => void;
-  fetchSelects: () => Promise<ServiceOrderStoreData['selects']>;
+  setSelects: (data: ServiceOrderStoreData['selects']) => void
+  fetchSelects: () => Promise<ServiceOrderStoreData['selects']>
 
-  viewMode: 'kanban' | 'grid';
-  setViewMode: (viewMode: 'kanban' | 'grid') => void;
+  viewMode: 'kanban' | 'grid'
+  setViewMode: (viewMode: 'kanban' | 'grid') => void
 }
 
 export const useServiceOrder = create<ServiceOrderStoreData>((set) => {
@@ -91,15 +99,15 @@ export const useServiceOrder = create<ServiceOrderStoreData>((set) => {
     viewMode: 'kanban',
 
     setStatusFilterValue(data) {
-      set({ statusFilterValue: data });
+      set({ statusFilterValue: data })
     },
 
     setStatusFilterData(data) {
-      set({ statusFilterData: data });
+      set({ statusFilterData: data })
     },
 
     setSelects(data) {
-      set({ selects: { ...data } });
+      set({ selects: { ...data } })
     },
 
     async fetchSelects() {
@@ -127,74 +135,87 @@ export const useServiceOrder = create<ServiceOrderStoreData>((set) => {
         await api
           .get('/system/choices/maintainers')
           .then((res) => res.data.data),
-      ]);
+      ])
 
       const result = {
         branch,
-        equipment: allEquipment.map((value: {
-          id: number;
-          equipmentCode: string;
-          description: string;
-          branch: SelectData;
-        }) => {
-          return {
-            value: value.id.toString(),
-            label: `${value.equipmentCode} - ${value.description}`,
-            branch: value.branch,
-          };
-        }),
+        equipment: allEquipment.map(
+          (value: {
+            id: number
+            equipmentCode: string
+            description: string
+            branch: SelectData
+          }) => {
+            return {
+              value: value.id.toString(),
+              label: `${value.equipmentCode} - ${value.description}`,
+              branch: value.branch,
+            }
+          },
+        ),
         typeMaintenance: allTypeMaintenance,
         maintenanceSector: allMaintenanceSector,
         status: allStatus, // Atualizado
         requester: allListRequester,
         maintainers: allMaintainers,
-      };
+      }
 
-      set({ selects: result });
+      set({ selects: result })
 
-      return result;
+      return result
     },
 
     async fetchServiceOrders() {
-      const response = await api.get('/maintenance/service-order/list-table');
-      const serviceOrders = response.data.rows;
-      set({ serviceOrders });
+      const response = await api.get('/maintenance/service-order/list-table', {
+        params: {
+          index: null,
+          perPage: null, // enviar false
+        },
+      })
+      const serviceOrders = response.data.rows
+      set({ serviceOrders })
     },
 
-   async updateServiceOrderStatus(orderId: number, newStatusId: number, additionalData?: { justify?: string, dateEnd?: string | null }) {
-  try {
-    const updatedOrderData: any = {
-      idStatusServiceOrder: newStatusId,
-      ...additionalData
-    };
-
-    const response = await api.put(`/maintenance/service-order/${orderId}/status`, updatedOrderData);
-
-    console.log('Status atualizado com sucesso:', response.data);
-
-    set((state) => {
-      const updatedOrders = state.serviceOrders?.map((order) => {
-        if (order.id === orderId) {
-          return {
-            ...order,
-            statusOrderService: {
-              ...order.statusOrderService,
-              id: newStatusId,
-            },
-          };
+    async updateServiceOrderStatus(
+      orderId: number,
+      newStatusId: number,
+      additionalData?: { justify?: string; dateEnd?: string | null },
+    ) {
+      try {
+        const updatedOrderData: any = {
+          idStatusServiceOrder: newStatusId,
+          ...additionalData,
         }
-        return order;
-      });
-      return { serviceOrders: updatedOrders };
-    });
-  } catch (error) {
-    console.error("Erro ao atualizar o status da ordem de serviço:", error);
-  }
-},
 
-    
-    setViewMode(viewMode) {
-      set({ viewMode });
+        const response = await api.put(
+          `/maintenance/service-order/${orderId}/status`,
+          updatedOrderData,
+        )
+
+        console.log('Status atualizado com sucesso:', response.data)
+
+        set((state) => {
+          const updatedOrders = state.serviceOrders?.map((order) => {
+            if (order.id === orderId) {
+              return {
+                ...order,
+                statusOrderService: {
+                  ...order.statusOrderService,
+                  id: newStatusId,
+                },
+              }
+            }
+            return order
+          })
+          return { serviceOrders: updatedOrders }
+        })
+      } catch (error) {
+        console.error('Erro ao atualizar o status da ordem de serviço:', error)
+      }
     },
-  };
-});
+
+    setViewMode(viewMode) {
+      set({ viewMode })
+    },
+  }
+})
